b87a462ecd89186bd2d73e458cadfc995a0916ac
Michel Donais <michel@misoservices.com>
2020-02-23 23:44:08 -0500

    Incremented package minimal versions for ones I can verify.

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "MisoDefaultsWrapper",
-    platforms: [.macOS(.v10_12),
-                .iOS(.v10),
-                .tvOS(.v10),
-                .watchOS(.v3)],
+    platforms: [.macOS(.v10_15),
+                .iOS(.v13),
+                .tvOS(.v13),
+                .watchOS(.v6)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
